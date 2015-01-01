ad9f4ea0eb98a09d93627129c5972b8f5ec87591
piwi <me@e-piwi.fr>
2015-01-01 21:28:37 +0100

    fix a typo in license begin year

--- src/I18n/aliases.php
+++ src/I18n/aliases.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Internationalization package.
  *
- * Copyleft (ↄ) 2013-2015 Pierre Cassat <me@e-piwi.fr> and contributors
+ * Copyleft (ↄ) 2010-2015 Pierre Cassat <me@e-piwi.fr> and contributors
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
